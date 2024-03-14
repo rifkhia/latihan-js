d0313ff98e5a3680130165581d6ac5572cbf7392
rajestika <rajestika@gmail.com>
2024-03-14 19:18:31 +0700

    Tambah latihan hari-2

---
+++ Hari-2/latihan.js
@@ -0,0 +1,22 @@
+/*
+Create a function that takes input a number (1 - 3,999)
+Then return the roman number of it
+example:
+console.log(toRomanNumber(167)) => CLXVII
+console.log(toRomanNumber((69)) => LXIX
+*/
+
+function toRomanNumber(n){
+}
+
+/*
+Create a function that takes input of any number
+Then return if the number prime or not
+If the number prime return true
+example:
+console.log(isPrime(8)) => false
+console.log(isPrime(17)) => true
+*/
+
+function isPrime(n){
+}
